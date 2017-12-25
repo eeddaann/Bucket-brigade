58b53f5880bb138b89f7a3ae43b2be53146c4a0b
eeddaann <edan100@gmail.com>
2017-12-25 17:08:46 +0200

    WIP

--- bb.py
+++ bb.py
@@ -2,7 +2,7 @@ import numpy as np
 
 HOURS = 8
 DAYS = 18
-DISCRETIZATION_FACTOR = 1  # the larger, the more accurate, defines the atomic time unit of the simulation. for example 60 means that each iteration in the model is a second.
+
 
 
 class Picker:
@@ -10,27 +10,44 @@ class Picker:
     defines a employee
     '''
 
-    def __init__(self, tf_mu, tf_variance, tb_mu, tb_variance, tp_mu, tp_variance, name):
-        self.position = None
-        self.cur_bucket = None
-        self.tf_mu = tf_mu * DISCRETIZATION_FACTOR  # expected value forward
-        self.tf_sigma = np.sqrt(tf_variance) * DISCRETIZATION_FACTOR  # std forward
-        self.tb_mu = tb_mu * DISCRETIZATION_FACTOR  # expected value backward
-        self.tb_sigma = np.sqrt(tb_variance) * DISCRETIZATION_FACTOR  # std backward
-        self.tp_mu = tp_mu * DISCRETIZATION_FACTOR  # expected value picking
-        self.tp_sigma = np.sqrt(tp_variance) * DISCRETIZATION_FACTOR  # std picking
+    def __init__(self, tf_mu, tf_variance, tb_mu, tb_variance, tp_mu, tp_variance):
+        self.rank = None  # relative position of picker to others
+        self.cur_pick_face = None  # reference to the current pick face
         self.cur_batch = None  # reference to the batch the employee make now
-
+        self.tf_mu = tf_mu  # expected value forward
+        self.tf_sigma = np.sqrt(tf_variance)  # std forward
+        self.tb_mu = tb_mu# expected value backward
+        self.tb_sigma = np.sqrt(tb_variance)  # std backward
+        self.tp_mu = tp_mu  # expected value picking
+        self.tp_sigma = np.sqrt(tp_variance)  # std picking
 
     def get_forward_time(self):
         # sample time for task
-        return max(0,np.random.normal(self.tf_mu, self.tf_sigma))+0.01
+        return max(0, np.random.normal(self.tf_mu, self.tf_sigma)) + 0.01
+
     def get_backward_time(self):
         # sample time for task
         return max(0, np.random.normal(self.tb_mu, self.tb_sigma)) + 0.01
+
     def get_picking_time(self):
         # sample time for task
         return max(0, np.random.normal(self.tp_mu, self.tp_sigma)) + 0.01
+
+    def is_busy(self,now):
+        # checks if picker's doing something
+        pass
+
+    def pick_item(self):
+        #pick one item
+        pass
+
+    def check_behind_same_pickface(self):
+        #checks if there's someone behind who's free in the same pickface
+        pass
+
+    def transfer(self,other):
+        #makes transfer of bucket to other picker
+        pass
 
 
 
@@ -38,7 +55,8 @@ class Batch:
     def __init__(self, start, expected_time):
         self.original_list = []
 
-    def transfer(self, other):
+    def is_done(self):
+        #check if specific batch is done
         pass
 
 
@@ -95,10 +113,6 @@ class dispatcher:
 
 
 
-class configuration:
-    def __init__(self, bb, par):
-        self.config = {'bb': bb, 'par': par}
-
 
 class simulation:
     def __init__(self, config):
@@ -106,39 +120,49 @@ class simulation:
         self.disp = dispatcher(self.config)
         self.cur_products = {}
         self.daily_total = []
+        self.batch_list = [[1,2,2,3,6,5],[2,3,2,6,5,4],[1,5,3,1,2,5]]
+        emp1 = Picker(1, 0.2, 0.5, 0.1, 1.4, 2)
+        emp2 = Picker(1, 0.2, 0.5, 0.1, 1.4, 2)
+        emp3 = Picker(1, 0.2, 0.5, 0.1, 1.4, 2)
+        self.pickers=[emp1,emp2,emp3]
+
 
     def run_config(self):
         now = 0
-        daily_counter = 0
-        self.disp.new_day(now)
-        for emp in self.disp.employee_lst:
-            self.cur_products[emp.cur_batch] = emp
-        for day in range(DAYS):
-            for time_unit in range(HOURS * 60 * DISCRETIZATION_FACTOR):
-                for product in self.cur_products.keys():
-                    sorted_by_prog_old = sorted(self.disp.bb_order, key=lambda emp: emp.cur_batch.progress)
-                    product.update_state(now)
-                    self.disp.check_order(sorted_by_prog_old)
-                    if product.finished == True:
-                        emp = self.cur_products[product]
-                        self.disp.new_product(emp, now)
-                        del self.cur_products[product]
-                        self.cur_products[emp.cur_batch] = emp
-                        daily_counter += 1
+        #TODO: make changes
 
-                now += 1
-            self.daily_total.append(daily_counter)
-            daily_counter = 0
-            self.disp.new_day(now)
-
-        return self.daily_total
+        while len(self.batch_list)>0: #and everyone finish
+            for picker in self.pickers:
+                if picker.is_busy(now):
+                    break
+                if picker.cur_batch==None: #if batch is finished. Check in the beginning and in the end
+                    picker.cur_batch=self.batch_list[-1]
+                if picker.check_behind_same_pickface():
+                    picker.transfer(self,other)
+                    pass
+                if picker.cur_batch[picker.cur_pick_face]!=0:
+                    picker.pick_item()
+                    pass
 
 
-emp1 = Picker(1,0.2,0.5,0.1,1.4,2,'a')
-emp2 = Picker(1,0.2,0.5,0.1,1.4,2, 'b')
-emp3 = Picker(1,0.2,0.5,0.1,1.4,2, 'c')
 
-config_dict = {1: ("Parallel:1,2,3", configuration([], [emp1, emp2, emp3])),
+
+            pass
+
+
+
+
+
+        return now #time that loop took
+
+
+
+
+
+
+
+
+'''config_dict = {1: ("Parallel:1,2,3", configuration([], [emp1, emp2, emp3])),
                2: ("bb:1,2,Parallel:3", configuration([emp1, emp2], [emp3])),
                3: ("bb:1,3,Parallel:2", configuration([emp1, emp3], [emp2])),
                4: ("bb:3,2,Parallel:1", configuration([emp3, emp2], [emp1])),
@@ -150,7 +174,7 @@ config_dict = {1: ("Parallel:1,2,3", configuration([], [emp1, emp2, emp3])),
                10: ("bb:1,3,2", configuration([emp1, emp3, emp2], [])),
                11: ("bb:2,1,3", configuration([emp2, emp1, emp3], [])),
                12: ("bb:2,3,1", configuration([emp2, emp3, emp1], [])),
-               13: ("bb:3,1,2", configuration([emp3, emp1, emp2], []))}
+               13: ("bb:3,1,2", configuration([emp3, emp1, emp2], []))}'''
 
 
 def calc(config):
@@ -160,10 +184,9 @@ def calc(config):
 
 
 input = 0
-while input != 'x':
+'''while input != 'x':
     for key in config_dict:
         print(key, config_dict[key][0])
     input = raw_input("choose config: (1,2..,13. x to exit)")
 
-    print(config_dict[int(input)][0] + "->  " + str(calc(config_dict[int(input)][1])))
-
+    print(config_dict[int(input)][0] + "->  " + str(calc(config_dict[int(input)][1])))'''
